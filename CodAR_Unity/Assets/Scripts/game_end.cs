bd2253ce5491d52cc69c333dae97236139120882
J Van K <jeevankur.iit@gmail.com>
2019-10-31 01:27:40 +0530

    Added October Final Project

---
+++ CodAR_Unity/Assets/Scripts/game_end.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class game_end : MonoBehaviour
+{
+
+
+    public void mainMenu(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void retry(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void next(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
---
+++ CodAR_Unity/Assets/Scripts/link_flow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class link_flow : MonoBehaviour
+{
+    public int index;
+    public string command;
+    public GameObject connector;
+    public GameObject codecube;
+    public bool haveChild = false;
+    void Start()
+    {
+        index = 100;
+        connector.SetActive(false);
+        // GameObject zone = this.FindComponentInChildWithTag<MeshRenderer>("zone");
+        // zone.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.CompareTag("zone")){
+            codecube = col.gameObject.transform.parent.gameObject;
+            codecube.GetComponent<link_flow>().haveChild = true;
+            // index = codecube.GetComponent<link_flow>().index + 1;
+            // col.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            connector.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider col){
+        if(col.gameObject.CompareTag("zone")){
+            index = 100;
+            codecube.GetComponent<link_flow>().haveChild = false;
+            //col.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            connector.SetActive(false);
+        }
+    }
+}
