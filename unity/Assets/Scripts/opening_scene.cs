bfd7730be4f578cf91eac21d8c83b728e118b9f8
J Van K <jeevankur.iit@gmail.com>
2020-01-02 13:10:44 +0530

    Upload new files

---
+++ unity/Assets/Scripts/opening_scene.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class opening_scene : MonoBehaviour
+{
+        public Button[] levels;
+        private int click = 0;
+
+        public void levelUpdate(){
+            int current = PlayerPrefs.GetInt("level", 0);
+            int i = 0;
+            foreach (Button level in levels){
+                if (i <= current){
+                    level.interactable = true;
+                }
+                else{
+                    level.interactable = false;
+                }
+                i++;
+            }
+        }
+
+        public void play(int i)
+        {
+            SceneManager.LoadScene(i);
+        }
+
+        public void clickAll(){
+            click++;
+            if(click%7==0){
+                unlockAll();
+            }
+            else if(click%13==0){
+                lockAll();
+            }
+        }
+
+        private void unlockAll(){
+            PlayerPrefs.SetInt("level", 12);
+        }
+
+        private void lockAll(){
+            PlayerPrefs.SetInt("level", 0);
+        }
+        
+
+}
---
+++ unity/Assets/Scripts/shaft_work.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shaft_work : MonoBehaviour {
+
+	public Animator anim;
+
+	void Start(){
+		anim = gameObject.GetComponent<Animator>();
+	}
+	void OnTriggerEnter (Collider col){
+		if(col.gameObject.tag == "Player"){
+			anim.SetBool("activate", true);
+		}
+		Debug.Log("Motor Connected");
+	}
+
+	void OnTriggerExit (Collider col){
+		if(col.gameObject.tag == "Player"){
+			anim.SetBool("activate", true);
+		}
+	}
+}
